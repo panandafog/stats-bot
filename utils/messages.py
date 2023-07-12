9e42e082454b5b48306c453028eef65d5971bdc6
panandafog <panandafog@gmail.com>
2023-07-13 00:07:30 +0300

    Update messages.py

--- utils/messages.py
+++ utils/messages.py
@@ -4,8 +4,13 @@ from stats.models import TopUsers
 
 
 def make_top_users_message(top_users: TopUsers):
-    text = f'Total time spent in voice chats since {top_users.start_datetime.strftime("%d/%m/%Y, %H:%M")}\n'
-    text += f'(first entry {top_users.first_entry.strftime("%d/%m/%Y, %H:%M")})\n'
+    start_str = top_users.start_datetime.strftime("%d/%m/%Y, %H:%M")
+    first_entry_str = top_users.first_entry.strftime("%d/%m/%Y, %H:%M")
+
+    text = f'Total time spent in voice chats since {start_str}\n'
+
+    if start_str != first_entry_str:
+        text += f'(first entry {first_entry_str})\n'
 
     for user_info in top_users.users_info:
         status = '💡' if user_info.is_active else ''
